f8ca8a27d04cc4f7b6fc4339623e9a8fed52f64d
Tô tử Siêu <sega@Tos-iMac.local>
2018-05-21 15:48:58 +0700

    Initial commit

---
+++ app/src/main/java/com/finger/hsd/activity/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.finger.hsd.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.finger.hsd.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
